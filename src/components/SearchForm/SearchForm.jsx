3428b7a31a046d68227495262cd8e924a5e8a54f
Victoria <berling2818@gmail.com>
2022-05-02 15:42:46 +0300

    Fetching data from GitHub with axios and useEffect

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,12 +1,38 @@
 import { Form, Input, FormWrapper } from "./SearchFormStyled";
 import { Loupe } from "../../assets";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchForm = () => {
+	const [data, setData] = useState([]);
+	const [userName, setUserName] = useState("");
+	const [url, setUrl] = useState();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await axios.get(url);
+
+			setData(data);
+		};
+
+		fetchData();
+	}, [url]);
+
 	return (
-		<Form>
+		<Form
+			onSubmit={(event) => {
+				setUrl(`https://api.github.com/users/${userName}`);
+				event.preventDefault();
+			}}
+		>
 			<FormWrapper>
 				<Loupe />
-				<Input type='text' placeholder='Enter GitHub username' />
+				<Input
+					type='text'
+					placeholder='Enter GitHub username'
+					value={userName}
+					onChange={(event) => setUserName(event.target.value)}
+				/>
 			</FormWrapper>
 		</Form>
 	);
